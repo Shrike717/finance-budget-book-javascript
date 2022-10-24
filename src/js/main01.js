bda7670330a25ff73e5916f0cf34c83c479ce475
Daniel Bauer <dan717@gmx.de>
2022-10-24 19:09:15 +0200

    Fix

--- src/js/main01.js
+++ src/js/main01.js
@@ -541,31 +541,6 @@ const haushaltsbuch = {
       })
   },
 
-  // // Method 03: Gesamtbilanz erstellen:
-  // gesamtbilanz_erstellen() {
-  //     let neue_gesamtbilanz = {
-  //         einnahmen: 0,
-  //         ausgaben: 0,
-  //         bilanz: 0,
-  //     };
-  //     this.eintraege.forEach(function (eintrag) {
-  //         switch (eintrag.typ) {
-  //             case "Einnahme":
-  //                 neue_gesamtbilanz.einnahmen += eintrag.betrag;
-  //                 neue_gesamtbilanz.bilanz += eintrag.betrag;
-  //                 break;
-  //             case "Ausgabe":
-  //                 neue_gesamtbilanz.ausgaben += eintrag.betrag;
-  //                 neue_gesamtbilanz.bilanz -= eintrag.betrag;
-  //                 break;
-  //             default:
-  //                 console.log(`Der Typ "${eintrag.typ}" ist nicht bekannt.`);
-  //                 break;
-  //           }
-  //     });
-  //     this.gesamtbilanz = neue_gesamtbilanz;
-  // },
-
    // Method 03: Gesamtbilanz erstellen:
    gesamtbilanz_erstellen() {
     let neue_gesamtbilanz = new Map();
@@ -590,15 +565,6 @@ const haushaltsbuch = {
     });
     this.gesamtbilanz = neue_gesamtbilanz;
 },
-
-  // // Method 04:Gesamtbilanz ausgeben
-  // gesamtbilanz_ausgeben() {
-  //     console.log(`Einnahmen:${this.gesamtbilanz.einnahmen} ct\n`
-  //       + `Ausgaben: ${this.gesamtbilanz.ausgaben} ct\n`
-  //       + `Bilanz: ${this.gesamtbilanz.bilanz} ct\n`
-  //       + `Bilanz ist positiv: ${this.gesamtbilanz.bilanz >= 0}`
-  //     );
-  // },
 
    // Method 04:Gesamtbilanz ausgeben
    gesamtbilanz_ausgeben() {
