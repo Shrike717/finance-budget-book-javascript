29847e500a42b49f31328127d8ca06cb54064379
Daniel Bauer <dan717@gmx.de>
2022-10-25 15:07:17 +0200

    Done datum timestamp

--- src/js/main01.js
+++ src/js/main01.js
@@ -495,6 +495,107 @@
 
 // NEUER CODE MIT OBJEKTEN ZU MAPS UND VERBESSERTEN METHODEN PRAXIS 10
 
+// // Objekt haushaltsbuch anlegen
+
+// const haushaltsbuch = {
+
+//   gesamtbilanz: new Map(),
+
+//   eintraege: [],
+
+//     // Method 01:Eingabedaten holen
+//     eintrag_erfassen() {
+
+//       let neuer_eintrag = new Map();
+
+//       neuer_eintrag.set("titel", prompt ("Titel:"));
+//       neuer_eintrag.set("typ", prompt ("Typ (Einnahme oder Ausgabe):"));
+//       neuer_eintrag.set("betrag", parseInt(prompt ("Betrag (in Cent):")));
+//       neuer_eintrag.set("datum", prompt ("Datum (jjjj-mm-tt):"));
+
+//       this.eintraege.push(neuer_eintrag);
+//   },
+
+//    // Method 1.1: Einträge nach Datum abbsteigend sortieren.
+//    eintraege_sortieren() {
+//     this.eintraege.sort(function (eintrag_a, eintrag_b) {
+//       if (eintrag_a.get("datum") > eintrag_b.get("datum")) {
+//           return -1;
+//       } else if (eintrag_a.get("datum") < eintrag_b.get("datum")){
+//           return 1;
+//       } else {
+//           return 0;
+//       };
+//   });
+// },
+
+//     // Method 02:Eingabedaten ausgeben
+//     eintraege_ausgeben() {
+//       console.clear();
+//       this.eintraege.forEach(function (eintrag) {
+//         console.log(`Titel: ${eintrag.get("titel")}\n`
+//             + `Typ: ${eintrag.get("typ")}\n`
+//             + `Betrag: ${eintrag.get("betrag")} ct\n`
+//             + `Datum: ${eintrag.get("datum")}`
+//           );
+//       })
+//   },
+
+//    // Method 03: Gesamtbilanz erstellen:
+//    gesamtbilanz_erstellen() {
+//     let neue_gesamtbilanz = new Map();
+//     neue_gesamtbilanz.set("einnahmen", 0);
+//     neue_gesamtbilanz.set("ausgaben", 0);
+//     neue_gesamtbilanz.set("bilanz", 0);
+
+//     this.eintraege.forEach(function (eintrag) {
+//         switch (eintrag.get("typ")) {
+//             case "Einnahme":
+//                 neue_gesamtbilanz.set("einnahmen", neue_gesamtbilanz.get("einnahmen") + eintrag.get("betrag"));
+//                 neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") + eintrag.get("betrag"));
+//                 break;
+//             case "Ausgabe":
+//                 neue_gesamtbilanz.set("ausgaben", neue_gesamtbilanz.get("ausgaben") + eintrag.get("betrag"));
+//                 neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") - eintrag.get("betrag"))
+//                 break;
+//             default:
+//                 console.log(`Der Typ "${eintrag.get("typ")}" ist nicht bekannt.`);
+//                 break;
+//           }
+//     });
+//     this.gesamtbilanz = neue_gesamtbilanz;
+// },
+
+//    // Method 04:Gesamtbilanz ausgeben
+//    gesamtbilanz_ausgeben() {
+//     console.log(`Einnahmen:${this.gesamtbilanz.get("einnahmen")} ct\n`
+//       + `Ausgaben: ${this.gesamtbilanz.get("ausgaben")} ct\n`
+//       + `Bilanz: ${this.gesamtbilanz.get("bilanz")} ct\n`
+//       + `Bilanz ist positiv: ${this.gesamtbilanz.get("bilanz") >= 0}`
+//     );
+// },
+
+//   // Funktion 05:Alle Funktionsaufrufe zusammengefasst in einer while Schleife
+//   eintrag_hinzufuegen() {
+//     let weiterer_eintrag = true;
+
+//     while (weiterer_eintrag) {
+//       this.eintrag_erfassen();
+//       this.eintraege_sortieren();
+//       this.eintraege_ausgeben();
+//       this.gesamtbilanz_erstellen();
+//       this.gesamtbilanz_ausgeben();
+//       weiterer_eintrag = confirm("Weiteren Eintrag hinzufügen?");
+//     }
+//   }
+// };
+
+// haushaltsbuch.eintrag_hinzufuegen();
+
+// console.log(haushaltsbuch);
+
+// NEUER CODE EEINTRÄGE MIT DATUM UND TIMESTAMP VERSEHEN PRAXIS 11
+
 // Objekt haushaltsbuch anlegen
 
 const haushaltsbuch = {
@@ -503,18 +604,19 @@ const haushaltsbuch = {
 
   eintraege: [],
 
-    // Method 01:Eingabedaten holen
+  // Method 01:Eingabedaten holen
     eintrag_erfassen() {
 
-      let neuer_eintrag = new Map();
+    let neuer_eintrag = new Map();
 
-      neuer_eintrag.set("titel", prompt ("Titel:"));
-      neuer_eintrag.set("typ", prompt ("Typ (Einnahme oder Ausgabe):"));
-      neuer_eintrag.set("betrag", parseInt(prompt ("Betrag (in Cent):")));
-      neuer_eintrag.set("datum", prompt ("Datum (jjjj-mm-tt):"));
+    neuer_eintrag.set("titel", prompt ("Titel:"));
+    neuer_eintrag.set("typ", prompt ("Typ (Einnahme oder Ausgabe):"));
+    neuer_eintrag.set("betrag", parseInt(prompt ("Betrag (in Cent):")));
+    neuer_eintrag.set("datum", new Date(prompt ("Datum (jjjj-mm-tt):") + " 00:00:00"));
+    neuer_eintrag.set("timestamp", Date.now());
 
-      this.eintraege.push(neuer_eintrag);
-  },
+    this.eintraege.push(neuer_eintrag);
+},
 
    // Method 1.1: Einträge nach Datum abbsteigend sortieren.
    eintraege_sortieren() {
@@ -529,17 +631,22 @@ const haushaltsbuch = {
   });
 },
 
-    // Method 02:Eingabedaten ausgeben
-    eintraege_ausgeben() {
-      console.clear();
-      this.eintraege.forEach(function (eintrag) {
-        console.log(`Titel: ${eintrag.get("titel")}\n`
-            + `Typ: ${eintrag.get("typ")}\n`
-            + `Betrag: ${eintrag.get("betrag")} ct\n`
-            + `Datum: ${eintrag.get("datum")}`
-          );
-      })
-  },
+
+  // Method 02:Eingabedaten ausgeben
+  eintraege_ausgeben() {
+    console.clear();
+    this.eintraege.forEach(function (eintrag) {
+      console.log(`Titel: ${eintrag.get("titel")}\n`
+          + `Typ: ${eintrag.get("typ")}\n`
+          + `Betrag: ${eintrag.get("betrag")} ct\n`
+          + `Datum: ${eintrag.get("datum").toLocaleDateString("de-DE", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          })}`
+        );
+    })
+},
 
    // Method 03: Gesamtbilanz erstellen:
    gesamtbilanz_erstellen() {
