a01998e4a25a889e95e3d509d8304b52d22dd5d6
Daniel Bauer <dan717@gmx.de>
2022-11-21 18:08:24 +0100

    Done monatsliste und monatslistensammlung III

--- src/js/classes/Monatsliste.js
+++ src/js/classes/Monatsliste.js
@@ -1,18 +1,5 @@
 "use strict";
 
-/*
-<article class="monatsliste">
-<h2>
-    <span class="monat-jahr">Februar 2020</span>
-    <span class="monatsbilanz negativ">-326,84€</span>
-</h2>
-<ul>
-    <li></li>
-    <li></li>
-</ul>
-</article>
-*/
-
 class Monatsliste {
 
     constructor(jahr, monat, ) {
@@ -24,11 +11,15 @@ class Monatsliste {
     }
 
     monat() {
-      this._monat = monat;
+        this._monat = monat;
     }
 
     jahr() {
-      this._jahr = jahr;
+        this._jahr = jahr;
+    }
+
+    bilanz() {
+      this._bilanz = 0;
     }
 
     eintrag_hinzufuegen(eintrag) {
@@ -42,21 +33,52 @@ class Monatsliste {
     //       });
     // }
 
-
-    //   // Methode: Oben generierte Listenpunkte in <ul> schiessen und diese in <article> schiessen. eintrag erzeugen.
-    //   _eintraege_anzeigen() {
-    //     // Überprüfen und Löschen von <ul>
-    //     document.querySelectorAll(".monatsliste ul").forEach(eintragsliste => eintragsliste.remove()); //Arrow
-
-    //     // Generieren und Platzieren von <ul> und oben generierten Listenpunkten
-    //     let eintragsliste = document.createElement("ul");
-    //     this._eintraege.forEach(eintrag => eintragsliste.insertAdjacentElement("beforeend", eintrag.html())); //Arrow
-    //     document.querySelector(".monatsliste").insertAdjacentElement("afterbegin", eintragsliste);
-    // }
+    /*
+    <article class="monatsliste">
+    <h2>
+        <span class="monat-jahr">Februar 2020</span>
+        <span class="monatsbilanz negativ">-326,84€</span>
+    </h2>
+    <ul>
+        <li></li>
+        <li></li>
+    </ul>
+    </article>
+    */
 
     _html_generieren() {
+        let monatsliste = document.createElement("article");
+        monatsliste.setAttribute("class", "monatsliste");
 
+        let ueberschrift = document.createElement("h2");
+
+        let monat_jahr = document.createElement("span");
+        monat_jahr.setAttribute("class", "monat-jahr");
+        monat_jahr.textContent = `${new Date(this._jahr, this._monat - 1).toLocaleString("de-De", {
+          month: "long",
+          year: "numeric"
+        })}`;
+        ueberschrift.insertAdjacentElement("afterbegin", monat_jahr);
+
+        let monatsbilanz = document.createElement("span");
+        monatsbilanz.setAttribute("class", "monatsbilanz");
+        if(this._bilanz >= 0) {
+            monatsbilanz.setAttribute("class", "monatsbilanz positiv");
+        } else {
+          monatsbilanz.setAttribute("class", "monatsbilanz negativ");
+        }
+        monatsbilanz.textContent = `${this.bilanz()} €`;
+
+        ueberschrift.insertAdjacentElement("beforend", monatsbilanz);
+
+        monatsliste.insertAdjacentElement("afterbegin", ueberschrift);
+
+        // Generieren und Platzieren von <ul> und oben generierten Listenpunkten
+        let eintragsliste = document.createElement("ul");
+        this._eintraege.forEach(eintrag => eintragsliste.insertAdjacentElement("beforeend", eintrag.html()));
+        monatsliste.insertAdjacentElement("beforeend", eintragsliste);
+
+        return monatsliste;
     }
-
 
 }
